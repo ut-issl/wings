e3a6748372afe13bd4784e3ce449c8d4c3f27ecc
TomokiMochizuki <mochizuki@space.t.u-tokyo.ac.jp>
2024-06-17 11:59:41 +0900

    remove deprecated reduxCreateStore

--- aspnetapp/WINGS/ClientApp/src/redux/store/store.ts
+++ aspnetapp/WINGS/ClientApp/src/redux/store/store.ts
@@ -1,9 +1,9 @@
 import {
-  createStore as reduxCreateStore,
   combineReducers,
   applyMiddleware,
   compose
 } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import * as H from 'history';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
@@ -22,20 +22,19 @@ declare var window: ExtendedWindow;
 const composeReduxDevToolsEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function createStore(history: H.History) {
-  return reduxCreateStore(
-    combineReducers({
-      router: connectRouter(history),
-      ui: UIReducer,
-      operation: OperationsReducer,
-      cmds: CommandsReducer,
-      tlms: TelemetriesReducer,
-      plans: PlansReducer,
-      views: ViewsReducer
-    }),
-    composeReduxDevToolsEnhancers(
-      applyMiddleware(
-        routerMiddleware(history),
-        thunk
-    ))
-  )
+  let reducer = combineReducers({
+    router: connectRouter(history),
+    ui: UIReducer,
+    operation: OperationsReducer,
+    cmds: CommandsReducer,
+    tlms: TelemetriesReducer,
+    plans: PlansReducer,
+    views: ViewsReducer
+  });
+  let enhancer = composeReduxDevToolsEnhancers(
+    applyMiddleware(
+      routerMiddleware(history),
+      thunk
+  ));
+  return configureStore({reducer})
 }
